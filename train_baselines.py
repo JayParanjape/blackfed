b8dd83520fbd2fdf38f5f99794f3b7aa0b0569ae
JayParanjape <jay.edutech@gmail.com>
2024-03-28 16:52:35 -0400

    improve logging

--- train_baselines.py
+++ train_baselines.py
@@ -12,6 +12,7 @@ from model import UNet
 from data_utils import get_data
 import yaml
 
+#define the loss function based on the input loss string
 class Loss_fxn():
     def __init__(self, losses_list=[]):
         if losses_list==[]:
@@ -30,7 +31,8 @@ class Loss_fxn():
         return loss
 
 
-def train(model, dataset_dict, save_path, loss_string, device, do_only_test=False):
+#training baselines includes the model without any split learning
+def train(model, dataset_dict, save_path, loss_string, device):
     model = model.to(device)
     os.makedirs(save_path, exist_ok=True)    
     #set up logger
@@ -51,137 +53,138 @@ def train(model, dataset_dict, save_path, loss_string, device, do_only_test=Fals
     loss_fxn = Loss_fxn(losses_list)
     print("debug: loss loaded")
 
-    if not do_only_test:
+    #control parameter for doing only testing
 
-        #set hyperparameters
-        num_epochs = 2000
-        bs = 8
-        lr = 1e-3
+    #set hyperparameters
+    num_epochs = 2000
+    bs = 8
+    lr = 1e-3
 
-        # tr_dataloader, val_dataloader = iter(dataloader_dict['train']), iter(dataloader_dict['val'])
-        tr_dataset, val_dataset, test_dataset = dataset_dict['train'], dataset_dict['val'], dataset_dict['test']
-        best_val_loss = 10000
-        best_tr_loss = 10000
-        # print("debug: len tr dataset", len(tr_dataset))
+    tr_dataset, val_dataset, test_dataset = dataset_dict['train'], dataset_dict['val'], dataset_dict['test']
+    best_val_loss = 10000
+    best_tr_loss = 10000
+    # print("debug: len tr dataset", len(tr_dataset))
 
-        #train model
-        best_val_loss = 100000000
-        best_tr_loss = 1000000000
-        for epoch in range(num_epochs):
+    #train model
+    best_val_loss = 100000000
+    best_tr_loss = 1000000000
+    for epoch in range(num_epochs):
+        running_loss = 0.0
+        running_intersection = 0
+        running_union = 0
+        running_corrects = 0
+        running_dice = 0
+        intermediate_count = 0
+        count = 0
+        preds_all = []
+        gold = []
+        hds = []
+        dataloaders = {}
+        dataloaders['train'] = torch.utils.data.DataLoader(dataset_dict['train'], batch_size=bs, shuffle=True, num_workers=4)
+        dataloaders['val'] = torch.utils.data.DataLoader(dataset_dict['val'], batch_size=bs, shuffle=False, num_workers=4)
+        optimizer = optim.AdamW(model.parameters(), lr=float(lr))
+
+        for inputs, labels,text_idxs, text in dataloaders['train']:
+            if len(labels.shape)==3:
+                labels = labels.unsqueeze(1)
+
+            count+=1
+            intermediate_count += inputs.shape[0]
+
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward
+            # track history if only in train
+            with torch.set_grad_enabled(True):
+                outputs = model(inputs)
+                loss=loss_fxn.forward(outputs, labels)
+                # print("Reg loss: ",reg_loss)
+                
+                # backward + optimize only if in training phase
+                loss.backward(retain_graph=False)
+                optimizer.step()
+
+            with torch.no_grad():
+                preds = (outputs>=0.5)
+
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+                ri, ru = running_stats(labels,preds)
+                running_dice += dice_collated(ri,ru)
+
+        #check training performance
+        if epoch%5==0:
+            epoch_loss = running_loss / ((count))
+            epoch_dice = running_dice / ((len(tr_dataset)))
+            print(f'Training at epoch {epoch} Train Loss: {epoch_loss:.4f} Train Dice: {epoch_dice:.4f}') 
+            logger.info('Training at epoch %d Train Loss: %f Train Dice: %f', epoch, epoch_loss, epoch_dice)
+
+        #check validation performance
+        if epoch%10==0:
             running_loss = 0.0
-            running_intersection = 0
-            running_union = 0
-            running_corrects = 0
             running_dice = 0
             intermediate_count = 0
             count = 0
-            preds_all = []
-            gold = []
             hds = []
-            dataloaders = {}
-            dataloaders['train'] = torch.utils.data.DataLoader(dataset_dict['train'], batch_size=bs, shuffle=True, num_workers=4)
-            dataloaders['val'] = torch.utils.data.DataLoader(dataset_dict['val'], batch_size=bs, shuffle=False, num_workers=4)
-            optimizer = optim.AdamW(model.parameters(), lr=float(lr))
-
-            for inputs, labels,text_idxs, text in dataloaders['train']:
+            for inputs, labels,text_idxs, text in dataloaders['val']:
                 if len(labels.shape)==3:
                     labels = labels.unsqueeze(1)
-
                 count+=1
                 intermediate_count += inputs.shape[0]
 
                 inputs = inputs.to(device)
                 labels = labels.to(device)
 
-                # zero the parameter gradients
-                optimizer.zero_grad()
-
                 # forward
                 # track history if only in train
-                with torch.set_grad_enabled(True):
+                with torch.no_grad():
                     outputs = model(inputs)
                     loss=loss_fxn.forward(outputs, labels)
                     # print("Reg loss: ",reg_loss)
                     
-                    # backward + optimize only if in training phase
-                    loss.backward(retain_graph=False)
-                    optimizer.step()
-
-                with torch.no_grad():
                     preds = (outputs>=0.5)
-                    # preds_all.append(preds.cpu())
-                    # gold.append(labels.cpu())
-                    # epoch_dice = dice_coef(preds,labels)
-                    # if count%100==0:
-                    #     print('iteration dice: ', epoch_dice)
 
 
                     # statistics
                     running_loss += loss.item() * inputs.size(0)
                     ri, ru = running_stats(labels,preds)
                     running_dice += dice_collated(ri,ru)
-                    
-            if epoch%5==0:
-                # print(count)
-                # print(running_loss, intermediate_count)
-                # print(running_loss/intermediate_count)
-                epoch_loss = running_loss / ((count))
-                epoch_dice = running_dice / ((len(tr_dataset)))
-                # epoch_dice = dice_coef(torch.cat(preds_all,axis=0),torch.cat(gold,axis=0))
-                print(f'Training at epoch {epoch} Train Loss: {epoch_loss:.4f} Train Dice: {epoch_dice:.4f}') 
-
-            if epoch%10==0:
-                running_loss = 0.0
-                running_dice = 0
-                intermediate_count = 0
-                count = 0
-                hds = []
-                for inputs, labels,text_idxs, text in dataloaders['val']:
-                # for inputs, labels,text_idxs, text, pt, pt_label in dataloaders[phase]:
-                    if len(labels.shape)==3:
-                        labels = labels.unsqueeze(1)
-                    count+=1
-                    intermediate_count += inputs.shape[0]
-
-                    inputs = inputs.to(device)
-                    labels = labels.to(device)
-
-                    # forward
-                    # track history if only in train
-                    with torch.no_grad():
-                        outputs = model(inputs)
-                        loss=loss_fxn.forward(outputs, labels)
-                        # print("Reg loss: ",reg_loss)
-                        
-                        preds = (outputs>=0.5)
-
-
-                        # statistics
-                        running_loss += loss.item() * inputs.size(0)
-                        ri, ru = running_stats(labels,preds)
-                        running_dice += dice_collated(ri,ru)
-                        hd = compute_hd95(preds, labels)
-                        if not math.isnan(hd):
-                            hds.append(hd)
-        
-                #validation performance
-                # print("HD95 avg: ", torch.mean(torch.Tensor(hds)))
-                # print(running_loss, intermediate_count)
-                # print(running_loss/intermediate_count)
-                epoch_loss = running_loss / ((count))
-                epoch_dice = running_dice / ((len(tr_dataset)))
-                # epoch_dice = dice_coef(torch.cat(preds_all,axis=0),torch.cat(gold,axis=0))
-                print(f'Training model at epoch {epoch} Validation Loss: {epoch_loss:.4f} Validation Dice: {epoch_dice:.4f} HD95 avg: {torch.mean(torch.Tensor(hds))}') 
-                
-                #save model
-                if epoch_loss < best_val_loss:
-                    best_val_loss = epoch_loss
-                    torch.save(model.state_dict(), os.path.join(save_path,'model_best_val.pth'))
+                    hd = compute_hd95(preds, labels)
+                    if not math.isnan(hd):
+                        hds.append(hd)
     
-    else:
-        model.load_state_dict(torch.load(os.path.join(save_path,'model_best_val.pth')), strict=True)
+            #validation performance
+            epoch_loss = running_loss / ((count))
+            epoch_dice = running_dice / ((len(dataset_dict['val'])))
+            print(f'Validating model at epoch {epoch} Validation Loss: {epoch_loss:.4f} Validation Dice: {epoch_dice:.4f} HD95 avg: {torch.mean(torch.Tensor(hds))}') 
+            logger.info('Validating model at epoch %d Validation Loss: %f Validation Dice: %f HD95 avg: %f', epoch, epoch_loss, epoch_dice, torch.mean(torch.Tensor(hds))) 
+            
+            #save model
+            if epoch_loss < best_val_loss:
+                best_val_loss = epoch_loss
+                torch.save(model.state_dict(), os.path.join(save_path,'model_best_val.pth'))
     
+    
+def test(model, dataset_dict, load_path, loss_string, device):
     #test on test dataset
+    model = model.to(device)
+    model.load_state_dict(torch.load(load_path), strict=True)
+
+    #define loss function
+    losses_list = []
+    if 'focal' in loss_string:
+        losses_list.append(focal_loss)
+    if 'dice' in loss_string:
+        losses_list.append(dice_loss)
+    if 'bce' in loss_string:
+        losses_list.append(nn.BCELoss())
+    loss_fxn = Loss_fxn(losses_list)
+    print("debug: loss loaded")
+
     running_loss = 0.0
     running_dice = 0
     intermediate_count = 0
@@ -221,18 +224,31 @@ def train(model, dataset_dict, save_path, loss_string, device, do_only_test=Fals
     epoch_loss = running_loss / ((count))
     epoch_dice = running_dice / ((len(dataset_dict['test'])))
     # epoch_dice = dice_coef(torch.cat(preds_all,axis=0),torch.cat(gold,axis=0))
-    print(f'Testing model at epoch {epoch} Test Loss: {epoch_loss:.4f} Test Dice: {epoch_dice:.4f} HD95 avg: {torch.mean(torch.Tensor(hds))}') 
+    print(f'Testing model Test Loss: {epoch_loss:.4f} Test Dice: {epoch_dice:.4f} HD95 avg: {torch.mean(torch.Tensor(hds))}') 
 
     return model
 
 
 if __name__ == '__main__':
+
+    device = sys.argv[1]
+    center_num = (sys.argv[2])
+    only_test = (sys.argv[3]=='True')
+    
     model = UNet(n_channels=3, n_classes=1)
-    with open('data_configs/isic.yml', 'r') as f:
+    # with open('data_configs/isic.yml', 'r') as f:
+    with open('data_configs/polypgen.yml', 'r') as f:
+
         config = yaml.load(f, Loader=yaml.FullLoader)
-    if sys.argv[2]=='super':
+    if center_num=='super':
         dataset_dict = get_data(config, center_num='super')
     else:
-        dataset_dict = get_data(config, center_num=int(sys.argv[2]))
+        dataset_dict = get_data(config, center_num=int(center_num))
 
-    model = train(model, dataset_dict, save_path = './baseline_'+str(sys.argv[2])+'/', loss_string='bce + dice', device=sys.argv[1], do_only_test=True)
+    #only train
+    if not only_test:
+        model = train(model, dataset_dict, save_path = './baselines/polyp/polyp_baseline_'+str(center_num)+'/', loss_string='bce + dice', device=device)
+        model = test(model, dataset_dict, load_path = './baselines/polyp/polyp_baseline_'+str(center_num)+'/model_best_val.pth', loss_string='bce + dice', device=device)
+    else:
+        #only test - give load path for model instead of save path
+        model = test(model, dataset_dict, load_path = './baselines/skin/skin_baseline_super/model_best_val.pth', loss_string='bce + dice', device=device)
